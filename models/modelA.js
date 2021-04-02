7e1c28bd06e40c0e99a71514817ed1a6efe722b7
Mae Duphorne <maelduphorne@gmail.com>
2021-04-02 14:22:35 -0600

    Fix dinosaur object mistake

--- models/modelA.js
+++ models/modelA.js
@@ -1,10 +1,10 @@
 var dinosaur = {
-  var species: 'Tyrannosaurus rex',
-  var nickname: 'T Rex',
-  var weightInTons: 9,
-  var toothSizeInInches: 12,
-  var isTerrifying: true,
-  var isCarnivorous: true,
-  var bodyParts: ["head", "arms", "tail", "belly", "legs"],
-  var prey: ["Triceratops", "Edmontosaurus", "Corythosaurus"],
+  species: 'Tyrannosaurus rex',
+  nickname: 'T Rex',
+  weightInTons: 9,
+  toothSizeInInches: 12,
+  isTerrifying: true,
+  isCarnivorous: true,
+  bodyParts: ["head", "arms", "tail", "belly", "legs"],
+  prey: ["Triceratops", "Edmontosaurus", "Corythosaurus"],
 }

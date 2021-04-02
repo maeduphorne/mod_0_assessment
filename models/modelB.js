158edb58eb5c39b300f8b3690ce88b1dd294a018
Mae Duphorne <maelduphorne@gmail.com>
2021-04-02 14:19:26 -0600

    Add developer object

---
+++ models/modelB.js
@@ -0,0 +1,10 @@
+var developer = {
+  name: "John Doe",
+  title: "Senior Engineer",
+  yearsInCurrentJob: 3,
+  yearsAsDeveloper: 10,
+  enjoysCareer: true,
+  attendedTuring: false,
+  languages: ["ruby", "javascript", "css", "html"],
+  companiesOnResume: ["Google", "Amazon", "Netflix"],
+}

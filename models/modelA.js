964cc2b624d4363f4a1ee51c91f667d154811d1c
Mae Duphorne <maelduphorne@gmail.com>
2021-04-02 14:11:11 -0600

    Add dinosaur object

---
+++ models/modelA.js
@@ -0,0 +1,10 @@
+var dinosaur = {
+  var species: 'Tyrannosaurus rex',
+  var nickname: 'T Rex',
+  var weightInTons: 9,
+  var toothSizeInInches: 12,
+  var isTerrifying: true,
+  var isCarnivorous: true,
+  var bodyParts: ["head", "arms", "tail", "belly", "legs"],
+  var prey: ["Triceratops", "Edmontosaurus", "Corythosaurus"],
+}

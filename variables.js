8d2a6adffc32f77ebab177d3861abc0db925c4a1
Mae Duphorne <maelduphorne@gmail.com>
2021-04-02 13:58:16 -0600

    Complete program specific challenge

---
+++ variables.js
@@ -0,0 +1,9 @@
+var movie = "Harry Potter and the Chamber of Secrets";
+var isEntertaining = true;
+movie = "Harry Potter and the Prisoner of Azkaban";
+num4 = 2
+num6 = 5
+var product = num4 * num6;
+var favoriteFoods = ["pizza", "spaghetti", "taco"];
+favoriteFoods.shift();
+console.log(movie.length)
